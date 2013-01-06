3495ecf2c67a1890eb9cedddc58a80442816611d
sinourain <sinourain@gmail.com>
2013-01-06 01:46:58 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeeTcg::Application.config.secret_token = 'a2735568caf3c4c2f0b08c231a7ef7f8600ce8eb6c3c0991b827e503a6f3c026007b417e9f76c20085078fc091b29b29bff496f922484b7d89aaba39ae3b71ec'
